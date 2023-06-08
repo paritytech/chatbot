d767ccf85bf0b31cff3341059e8430cdf5a1b0fc
Javier Bullrich <javier@bullrich.dev>
2023-06-08 14:43:53 +0200

    created adapter for edge functions
    It should work super fast now

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import nodeAdapter from '@sveltejs/adapter-node';
+import vercelAdapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,10 +15,8 @@ const config = {
 	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// adapter set up automatically based on the platform
+		adapter: process.env.VERCEL ? vercelAdapter({ runtime: 'edge' }) : nodeAdapter()
 	}
 };
 
