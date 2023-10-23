7f50782e383197a65885dcd350fa768c51e5acff
Javier Bullrich <javier@bullrich.dev>
2023-10-23 11:01:15 +0200

    added source of data to db

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,41 +1,9 @@
 import * as dotenv from 'dotenv';
-import url from 'url';
 
+import { generateEmbeddings } from './createEmbeddings';
 import { fetchDocs } from './fetch';
-import { Embeddings, generateEmbeddings } from './createEmbeddings';
-import { createClient } from 'redis';
-import { LocalIndex } from 'vectra';
-import Openai from 'openai';
-import path from 'path';
 
 dotenv.config();
-
-const storeEmbeddings = async (embeddings: { [key: string]: Embeddings }) => {
-	const dbLocation = path.join(
-		path.parse(url.fileURLToPath(import.meta.url)).dir,
-		'..',
-		'data/embedings'
-	);
-
-	console.log('Connecting to index in', dbLocation);
-
-	const index = new LocalIndex(dbLocation);
-
-	if (!(await index.isIndexCreated())) {
-		console.log('Index is not created. Creating new one');
-		await index.createIndex();
-	}
-
-	const keys = Object.keys(embeddings);
-	for (let i = 0; i < keys.length; i++) {
-		console.log(`Saving to disk 💾 embedding ${i}/${keys.length}`);
-		const key = keys[i];
-		await index.insertItem({
-			vector: embeddings[key].embedding,
-			metadata: { text: key }
-		});
-	}
-};
 
 const runSetup = async () => {
 	console.log(
@@ -44,9 +12,7 @@ const runSetup = async () => {
 
 	await fetchDocs();
 
-	const embeddings = await generateEmbeddings();
-
-	storeEmbeddings(embeddings);
+	await generateEmbeddings();
 };
 
 runSetup().then(() => console.log('Done 💫'));
