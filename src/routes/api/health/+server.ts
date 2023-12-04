af67801d8e4bf52fe0c4aa0ff3f4aa97b2579ade
Javier Bullrich <javier@bullrich.dev>
2023-12-04 11:54:41 -0300

    removed weaviate client from health endpoint

--- src/routes/api/health/+server.ts
+++ src/routes/api/health/+server.ts
@@ -1,5 +1,4 @@
 import { OPENAI_API_KEY } from '$env/static/private';
-import { weaviateClient } from '$lib/server/chatbot';
 import OpenAI from 'openai';
 import type { RequestHandler } from './$types';
 
@@ -7,9 +6,5 @@ export const GET: RequestHandler = async () => {
 	const openAi = new OpenAI({ apiKey: OPENAI_API_KEY });
 	const models = await openAi.models.list();
 	console.log('There are %s available OpenAI models', models.data.length);
-	const result = await weaviateClient.schema.getter().do();
-	if (!result) {
-		throw new Error('Weaviate not configured correctly!');
-	}
 	return new Response('Ok', { status: 200 });
 };
