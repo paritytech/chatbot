3a4a3dd7b0085a0a39a8d07198279ebbfd5c6e96
Javier Bullrich <javier@bullrich.dev>
2023-12-04 16:21:49 -0300

    added script to get stackexchange answers
    It only works with the top 99 votes answers

---
+++ scripts/fetchStackExchange.ts
@@ -0,0 +1,139 @@
+import axios from "axios";
+import { mkdirSync } from "fs";
+
+import { writeFile } from "fs/promises";
+
+interface QuestionWithBody {
+    is_answered: boolean;
+    view_count: number;
+    accepted_answer_id: number;
+    score: number;
+    answer_count: number;
+    question_id: number;
+    link: string;
+    title: string;
+    body: string;
+    body_markdown: string;
+}
+
+interface Answer {
+    is_accepted: boolean;
+    score: number;
+    answer_id: number;
+    question_id: number;
+    body: string;
+    body_markdown: string;
+}
+
+interface StackExchangeInfo {
+    question_id: number;
+    answer_id: number;
+    title: string;
+    body: string;
+    answer: string;
+    url: string;
+    answer_url: string;
+}
+
+const site = "substrate.stackexchange";
+
+const axiosGet = async<T = any>(url: string, params: URLSearchParams): Promise<T> => {
+    const requestUrl = url + "?" + params.toString();
+    try {
+        console.log("About to get 📃 from:", requestUrl);
+        const { data } = await axios.get<T>(requestUrl);
+
+        console.debug(data);
+
+        return data;
+    }
+    catch (error: any) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.error(error.response.data);
+            console.error(error.response.status);
+            throw new Error(error.response.data);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser 
+            // and an instance of http.ClientRequest in node.js
+            console.error(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.error('Error', error.message);
+        }
+        throw error;
+    }
+}
+
+/**
+ * Fetch the top 99 Stack Exchange questions and writes them down into a markdown file
+ */
+export const fetchQuestions = async (): Promise<string> => {
+    const url = "https://api.stackexchange.com/2.3/questions";
+
+    const params = new URLSearchParams();
+    params.append('pagesize', '99');
+    params.append('order', 'desc');
+    params.append('sort', 'votes');
+    params.append('site', site);
+    params.append('filter', '!1ZFzxYL36KQNcy0oOKFWC)yuuGWzdzWPZ');
+
+    const questions = await axiosGet<{ items: QuestionWithBody[] }>(url, params);
+
+    console.log(questions.items[0]);
+
+    const answerParams = new URLSearchParams();
+    answerParams.append('site', site);
+    answerParams.append('filter', '!-)QWsboN0d_T');
+
+    mkdirSync('data/se/', { recursive: true });
+
+    const answers: StackExchangeInfo[] = [];
+
+    for (const question of questions.items.filter(q => q.is_answered)) {
+        if (!question.accepted_answer_id) {
+            console.log("Skipping", question.question_id, question.title, "because it does not have an accepted answer");
+            continue;
+        }
+        const url = "https://api.stackexchange.com/2.3/answers/" + question.accepted_answer_id;
+        console.log("Getting answer from: '%s'", question.title);
+        const answer = await axiosGet<{ items: Answer[] }>(url, answerParams);
+        console.log("ANSWER!", answer.items);
+        const pair: StackExchangeInfo = {
+            question_id: question.question_id,
+            answer_id: question.accepted_answer_id,
+            title: question.title,
+            body: question.body_markdown,
+            answer: answer.items[0].body_markdown,
+            url: question.link,
+            answer_url: `https://${site}.com/a/${question.accepted_answer_id}`
+        }
+        await writeFile(`data/se/${question.question_id}.json`, JSON.stringify(pair, null, 2));
+        answers.push(pair);
+    }
+    console.log(questions.items.map(q => q.score));
+
+    const combination = answers.map(answer => `# ${answer.title}
+
+## Question
+
+${answer.body}
+
+## Answer
+
+${answer.answer}
+
+## Source
+
+${answer.answer_url}`).join("\n\n---\n\n");
+
+    const combinedFile = "data/stackExchangeCombinedDoc.md";
+
+    await writeFile(combinedFile,
+        "This document contains answers from https://substrate.stackexchange.com. They are separated with a '---'\n\n" +
+        combination);
+
+    return combinedFile;
+}
--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,14 +1,17 @@
 import { createAssistant } from './assistant.js';
 import { fetchDocs } from './fetch.js';
+import { fetchQuestions } from './fetchStackExchange.js';
 
 const runSetup = async () => {
 	console.log(
 		'Hello! ✨. This script will download the doc files and generate the embeddings for them!'
 	);
 
-	const fileName = await fetchDocs();
+	const docsFile = await fetchDocs();
 
-	await createAssistant([fileName]);
+	const stackExchangeFile = await fetchQuestions();
+
+	await createAssistant([docsFile, stackExchangeFile]);
 };
 
 runSetup().then(() => console.log('Done 💫'));
